41c9b2f1d8e23d9218a9bfebcd64ab28b06b0f47
Muhammad Ahtisham Arif <ahtisham0100@gmail.com>
2025-07-12 18:36:30 +0500

    shadcn

--- understandingComponentBasedArchtecture/src/App.jsx
+++ understandingComponentBasedArchtecture/src/App.jsx
@@ -3,12 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from './components/ui/button'
+import { ModeToggle } from './components/ui/ModeToggle'
+import { ThemeProvider } from './components/ui/ThemeProvider'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <ModeToggle/> 
     <h1>Counter: {count}</h1>
 <Button variant="default"  size="lg" onClick={() => setCount(count + 1)}>
 {count} Click me
@@ -20,6 +23,15 @@ function App() {
 <Button variant={"secondary"} className="ml-4" onClick={()=>{
   document.body.classList.toggle('dark')
 }}> Switch theme </Button>
+
+<h1>understand the aschild in buttons</h1>
+<Button asChild variant={"default"} size={"lg"}>
+   <a href="#">This is child</a>
+</Button> 
+
+<Button asChild onClick={()=>{alert("button clicked")}}>
+  <div className="btn-div">This is a btn div</div>
+</Button>
 </>
   )
 }
--- understandingComponentBasedArchtecture/src/main.jsx
+++ understandingComponentBasedArchtecture/src/main.jsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ThemeProvider } from './components/ui/ThemeProvider.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+  <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >    <App />
+        </ThemeProvider>
+
   </StrictMode>,
 )
